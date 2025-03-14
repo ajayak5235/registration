e6d5287a71ec0ae9f34c7f999cedfb83e45c123d
ajayak5235 <131739918+ajayak5235@users.noreply.github.com>
2025-03-14 15:43:23 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,17 @@
+
 const express = require('express');
 require('dotenv').config();
-// const bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 const cors = require('cors');
 const connectDB = require('./utily/database') 
 const userRoutes = require('./routers/userRoute');
 
 const app = express();
-app.use(express.json());
+app.use(bodyParser.json());
 app.use(cors());
 
-// app.use('/', (req,res) =>{
-//     res.send("Server is Running")
-// })
+
+
 // Use user routes
 app.use('/api', userRoutes);
 
